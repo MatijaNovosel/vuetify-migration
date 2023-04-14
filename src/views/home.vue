dce4ad85923f937c0cd3f4d066ec95f1d6911c4a
Matija Novosel <mnovosel5@gmail.com>
2023-04-14 12:41:41 +0200

    Tweak

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="d-flex flex-column justify-center items-center h-100 text-center">
     <span> Welcome to the home page </span>
-    {{ state.date }}
     <div class="mt-5 d-flex justify-center">
       <v-date-picker
         max="2020-04-12"
