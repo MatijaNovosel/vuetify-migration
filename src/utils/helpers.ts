2c58268746902b9e699cb4c32b6ca941774aceca
Matija Novosel <mnovosel5@gmail.com>
2023-04-20 14:13:52 +0200

    Unused helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,33 +2,6 @@ export function kebabCase(str: string): string {
   return (str || "").replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 }
 
-export function getNestedValue(
-  obj: any,
-  path: (string | number)[],
-  fallback?: any
-): any {
-  const last = path.length - 1;
-  if (last < 0) return obj === undefined ? fallback : obj;
-  for (let i = 0; i < last; i++) {
-    if (obj == null) return fallback;
-    obj = obj[path[i]];
-  }
-  if (obj == null) return fallback;
-  return obj[path[last]] === undefined ? fallback : obj[path[last]];
-}
-
-export function getObjectValueByPath(
-  obj: any,
-  path: string,
-  fallback?: any
-): any {
-  if (obj == null || !path || typeof path !== "string") return fallback;
-  if (obj[path] !== undefined) return obj[path];
-  path = path.replace(/\[(\w+)\]/g, ".$1"); // convert indexes to properties
-  path = path.replace(/^\./, ""); // strip a leading dot
-  return getNestedValue(obj, path.split("."), fallback);
-}
-
 export function wrapInArray<T>(v: T | T[] | null | undefined): T[] {
   return v != null ? (Array.isArray(v) ? v : [v]) : [];
 }
